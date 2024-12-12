b483396f6132b151a5a17630715ac439c4f22cb3
palwa-coder <palwaaj100@gmail.com>
2024-12-12 11:51:36 +0700

    bikin stat

---
+++ source/stat.h
@@ -0,0 +1,21 @@
+int playerhp = 100;
+int playermana = 50;
+int playerattack = 4;
+int playermagic = 8;
+int playerdefense = 2;
+int playermagicdefense = 1;
+
+int enemyhp = 100;
+int enemyattack = 4;
+int enemymagic = 8;
+int enemydefense = 2;
+int enemymagicdefense = 1;
+
+int defenseattr = 0;
+int defenseattre = 0;
+
+char battlemodifierA;
+char battlemodifierB;
+
+int specialtracker;
+int specialtracker2 = 1;
